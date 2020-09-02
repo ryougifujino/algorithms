e5d4a5613f89b28565928cf5b6750c665e9c7330
ryougifujino <ryougifujino@gmail.com>
2020-09-02 14:01:08 +0800

    Path in Matrix

---
+++ offer/path-in-matrix.js
@@ -0,0 +1,86 @@
+/**
+ * @param {string[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+const exist = function (board, word) {
+    if (!word || word.length === 0) {
+        return false;
+    }
+
+    const rowMax = board.length - 1;
+    const colMax = board[0].length - 1;
+
+    const firstChar = word.charAt(0);
+    for (let row = 0; row <= rowMax; row++) {
+        for (let col = 0; col <= colMax; col++) {
+            // [row, col]：矩阵中和word第一个字符相等的点
+            if (board[row][col] === firstChar && go([row ,col], word.length)) {
+                return true;
+            }
+        }
+    }
+    return false;
+
+    function walkAround(startCoord, walk, leftSteps) {
+        const [nextValue, nextCood] = walk(startCoord);
+        if (nextValue !== undefined && nextValue !== '/') {
+            return go(nextCood, leftSteps - 1);
+        }
+        return false;
+    }
+
+    function go(startCoord, leftSteps) {
+        const nowChar = word.charAt(word.length - leftSteps);
+        const startCoordVal = coordVal(startCoord);
+        if (startCoordVal !== nowChar) {
+            return false;
+        }
+        // 目标坐标和目标值相等且步数等于最后一步，说明在矩阵中找到了路径
+        if (leftSteps === 1) {
+            return true;
+        }
+        setCoordVal(startCoord, '/');
+        const result = (
+            walkAround(startCoord, top, leftSteps) ||
+            walkAround(startCoord, right, leftSteps) ||
+            walkAround(startCoord, bottom, leftSteps) ||
+            walkAround(startCoord, left, leftSteps)
+        );
+        setCoordVal(startCoord, startCoordVal);
+        return result;
+    }
+
+    // helpers
+    function top(coord) {
+        const [row, col] = coord;
+        return [row === 0 ? undefined : board[row - 1][col], [row - 1, col]];
+    }
+    function right(coord) {
+        const [row, col] = coord;
+        return [col === colMax ? undefined : board[row][col + 1], [row, col + 1]];
+    }
+    function bottom(coord) {
+        const [row, col] = coord;
+        return [row === rowMax ? undefined : board[row + 1][col], [row + 1, col]];
+    }
+    function left(coord) {
+        const [row, col] = coord;
+        return [col === 0 ? undefined : board[row][col - 1], [row, col - 1]];
+    }
+    function coordVal(coord) {
+        const [row, col] = coord;
+        return board[row][col];
+    }
+    function setCoordVal(coord, val) {
+        board[coord[0]][coord[1]] = val;
+    }
+};
+
+let matrix = [['a']];
+let word = 'a';
+console.assert(exist(matrix, word) === true);
+
+matrix = [["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","b"]];
+word = "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+console.assert(exist(matrix, word) === true);
